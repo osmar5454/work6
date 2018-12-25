932ff2339b1cb5de6f137fcf7b59260504ae63b2
osmar5454 <daniwapi@outlook.com>
2018-12-25 11:33:10 -0500

    '1'

---
+++ lib/card.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+Widget carta(BuildContext context ,String color,String nombre ,String poder,String imagen,Color colorTexto){
+  int getColorHexFromStr(String colorStr) {
+    colorStr = "FF" + colorStr;
+    colorStr = colorStr.replaceAll("#", "");
+    int val = 0;
+    int len = colorStr.length;
+    for (int i = 0; i < len; i++) {
+      int hexDigit = colorStr.codeUnitAt(i);
+      if (hexDigit >= 48 && hexDigit <= 57) {
+        val += (hexDigit - 48) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 65 && hexDigit <= 70) {
+        // A..F
+        val += (hexDigit - 55) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 97 && hexDigit <= 102) {
+        // a..f
+        val += (hexDigit - 87) * (1 << (4 * (len - 1 - i)));
+      } else {
+        throw new FormatException("An error occurred when converting a color");
+      }
+    }
+    return val;
+  }
+  
+  
+  return  Container(
+    width: 400.0,
+    height: 275.0,
+    color: Colors.transparent,
+    child: Stack(
+      children: <Widget>[
+
+        Positioned(
+          top: 46.0,
+          left: 10.0,
+          child: Row(
+            children: <Widget>[
+              Text('Rates', style: TextStyle(fontFamily: 'Pacifico', fontSize: 13.0 , color: Color(getColorHexFromStr('#F9622F')) ,fontWeight: FontWeight.w500  ),),
+              SizedBox(width: 3.0,),
+              Icon(Icons.star , size: 13.0, color: Colors.black,),
+              Icon(Icons.star , size: 13.0, color: Colors.black,),
+              Icon(Icons.star , size: 13.0, color: Colors.black,),
+              Icon(Icons.star_half , size: 13.0, color: Colors.black,),
+              Icon(Icons.star_border , size: 13.0, color: Colors.black,),
+
+            ],
+          )
+        ),
+
+
+        Padding(
+          padding: EdgeInsets.only(left: 5.0 , right: 20.0,top: 70.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                color: Color(getColorHexFromStr(color))
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color(getColorHexFromStr(color)),
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(20.0),topLeft: Radius.circular(20.0),bottomLeft: Radius.circular(20.0),bottomRight: Radius.circular(20.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(getColorHexFromStr(color))   , blurRadius: 6.0,
+                    )
+                  ]
+              ),
+            ),
+          ),
+        ),
+
+
+
+        Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 170.0 , top: 80.0),
+              width: 200.0,
+              height: 10.0,
+              child: Text(nombre , style: TextStyle(fontFamily: 'Muli' ,fontWeight: FontWeight.w600 , fontSize: 25.0 ,color: colorTexto )),
+            ),
+            SizedBox(height: 35.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  color: Colors.transparent,
+                  margin: EdgeInsets.only(left: 15.0),
+                  width: 165.0,
+                  height: 90.0,
+                  child: Text(poder,style: TextStyle(color: colorTexto ,fontSize: 10.5, fontFamily: 'Muli'  ),),
+                ),
+              ],
+            ),
+            SizedBox(height: 3.0,),
+            Container(
+              margin: EdgeInsets.only(right: 175.0, top: 10.0),
+              padding: EdgeInsets.only(top: 2.0  , right: 125.0),
+              color: Colors.transparent,
+              width: 195.0,
+              height: 15.0,
+              child: Column(
+                children: <Widget>[
+                  Text('Comments   (24)' ,style: TextStyle(fontFamily: 'Muli' , fontWeight: FontWeight.w500 , fontSize:  9.0 , color: colorTexto),)
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 170.0, top: 0.0 , left:  13.0),
+              padding: EdgeInsets.only(top: 2.0  , right: 80.0),
+              color: Colors.transparent,
+              width: 220.0,
+              height: 15.0,
+              child: Row(
+                children: <Widget>[
+                  Text('BY' ,style: TextStyle(fontFamily: 'Muli' , fontWeight: FontWeight.w500 , fontSize:  9.0 , color: colorTexto),),
+                  Text(' Osmar' ,style: TextStyle(fontFamily: 'Muli' , fontWeight: FontWeight.w500 , fontSize:  9.0 , color: colorTexto),),
+                  Text(' Valenzuela' ,style: TextStyle(fontFamily: 'Muli' , fontWeight: FontWeight.w500 , fontSize:  9.0 , color: colorTexto),),
+                  Text(' Mendoza' ,style: TextStyle(fontFamily: 'Muli' , fontWeight: FontWeight.w500 , fontSize:  9.0 , color: colorTexto),),
+
+
+                ],
+              ),
+            ),
+          ],
+        ),
+
+
+        Positioned(
+          left: 170.0,
+          top: 0.0,
+          bottom: 60.0,
+          child: Container(
+
+            width: 250.0,
+            height: 250.0,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(imagen)),
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+
+
+      ],
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:work6/card.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int getColorHexFromStr(String colorStr) {
+    colorStr = "FF" + colorStr;
+    colorStr = colorStr.replaceAll("#", "");
+    int val = 0;
+    int len = colorStr.length;
+    for (int i = 0; i < len; i++) {
+      int hexDigit = colorStr.codeUnitAt(i);
+      if (hexDigit >= 48 && hexDigit <= 57) {
+        val += (hexDigit - 48) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 65 && hexDigit <= 70) {
+        // A..F
+        val += (hexDigit - 55) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 97 && hexDigit <= 102) {
+        // a..f
+        val += (hexDigit - 87) * (1 << (4 * (len - 1 - i)));
+      } else {
+        throw new FormatException("An error occurred when converting a color");
+      }
+    }
+    return val;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Column(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //---------------------Usuario---------------------------
+              Padding(
+                padding: EdgeInsets.only(left: 15.0 , top:20.0 ) ,
+                child: Column(
+                  children: <Widget>[
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40.0),
+                            color: Colors.transparent,
+                          ),
+                          child: Material(
+                            elevation: 30.0,
+                            color: Colors.transparent,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(40.0),
+                                image: DecorationImage( fit: BoxFit.cover,image: AssetImage('images/img12.png')),
+                              ),
+                            ),
+                          ),
+                        ),
+
+
+                        Row(children: <Widget>[
+                          Container(
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40.0),
+                              border: Border.all(color: Colors.grey.withOpacity(0.7))
+                            ),
+                            child: Icon(Icons.message , size: 20.0, color: Colors.grey,),
+                          ),
+                          SizedBox(width: 10.0,),
+                          Container(
+                            width: 120.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0) , 
+                              color: Color(getColorHexFromStr('#764CFF')),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(getColorHexFromStr('#764CFF')) , spreadRadius: 0.0 , blurRadius: 4.0
+                                )
+                              ]
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.add , size: 15.0 , color: Colors.black,),
+                                SizedBox(width: 5.0,),
+                                Container(
+                                  child: Text('FOLLOW' ,style: TextStyle(fontFamily: 'Comfortaa' , fontSize: 13.0 , fontWeight: FontWeight.bold ),),
+                                ),
+                              ],
+                            )
+                          ),
+                          SizedBox(width: 10.0,),
+
+                        ],)
+
+
+                      ],
+                    ),
+
+                    SizedBox(height: 30.0,),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Bienvenido, Osmar' , style:  TextStyle(fontFamily: 'Pacifico' , fontWeight: FontWeight.w200 , fontSize: 35.0 , color: Color(getColorHexFromStr('#F45D69'))),),
+                        Container(
+                          margin: EdgeInsets.only(top: 32.0 , left: 8.0),
+                          width: 9.0,
+                          height: 9.0,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.0) , color: Color(getColorHexFromStr('#38D85D'))),
+                        )
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.location_on , size:  15.0, color: Colors.grey.withOpacity(0.6),),
+                        Text('Beijin' , style: TextStyle(fontFamily: 'Muli', fontSize: 15.0 , fontWeight: FontWeight.w600 , color: Colors.grey.withOpacity(0.6)))
+                      ],
+                    ),
+                    SizedBox(height: 5.0,),
+                    Text('Los cambios no son buenos ni malos, son inevitables.', style: TextStyle(fontFamily: 'IndieFlower' , fontSize: 17.5 , color: Color(getColorHexFromStr('#011C33')) ),),
+
+                    SizedBox(height: 10.0,),
+                    Container(
+                      padding: EdgeInsets.only(left: 2.0 , right: 4.0),
+                      width: MediaQuery.of(context).size.width,
+                      child: Row(
+
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+
+                          Column(
+                            children: <Widget>[
+                              Text('1.123' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 18.0 ,fontWeight: FontWeight.w500 , color: Colors.black),),
+                              Text('LIKES' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 10.0 , fontWeight: FontWeight.w500 , color: Colors.grey.withOpacity(0.6)),)
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text('24K' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 18.0 ,fontWeight: FontWeight.w500 , color: Colors.black),),
+                              Text('FOLLOWERS' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 10.0 , fontWeight: FontWeight.w500 , color: Colors.grey.withOpacity(0.6)),)
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text('521' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 18.0 ,fontWeight: FontWeight.w500 , color: Colors.black),),
+                              Text('FOLLOWING' , style: TextStyle(fontFamily: 'Merriweather' , fontSize: 10.0 , fontWeight: FontWeight.w500 , color: Colors.grey.withOpacity(0.6)),)
+                            ],
+                          ),
+
+
+
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 25.0,),
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: <Widget>[
+                       Row(
+                         children: <Widget>[
+                           Container(
+                             width: MediaQuery.of(context).size.width/14 ,
+                             height: 5.0,
+                             color: Color(getColorHexFromStr('#011C33'),),
+                           ),
+                           SizedBox(width: 4.0,),
+                           Text('ALBUM' ,style: TextStyle(fontFamily: 'Comfortaa' , fontSize: 15.0 , color: Color(getColorHexFromStr('#011C33' ))  , fontWeight: FontWeight.bold ), )
+
+                         ],
+                       ),
+                       
+                       
+                      Row(
+                        children: <Widget>[
+                          Text('More'  , style: TextStyle(fontFamily: 'Comfortaa' , fontSize: 12.0 , color: Color(getColorHexFromStr('#270CD6' )) , fontWeight: FontWeight.bold ),),
+                          SizedBox(width: 2.0,),
+                          Icon(Icons.keyboard_arrow_right , size: 18.0, color: Color(getColorHexFromStr('#270CD6' )))
+                        ],
+                      ),
+                     ],
+                   ),
+
+
+                    carta(context, '#523ED6', 'Baby Dragon','Una unidad voladora que inflige daño de área. Cuando nacen, los bebes dragón son monísimos, pero también tienen mucha hambre y siempre les apetece un buen churrasco.','images/img1.png',Color(getColorHexFromStr('#FFFCF2'))),
+                    carta(context, '#F45D69'  , 'Wich' ,'Invoca esqueletos y dispara rayos destructores. Por desgracia, sus brillantes ojos de color rosa no disparan rayos láser.','images/img4.png',Colors.white),
+                    carta(context, '#FF9868'  , 'Minions' ,'Tres atacantes voladores, rápidos y desarmados. ¿Es un pájaro? ¿Es un avión? ¡No, es un superesbirro!','images/img10.png',Colors.white),
+                    carta(context, '#E3E858'  , 'P.E.K.K.A' ,'Un luchador cuerpo a cuerpo pesado y lento. Se contonea ligeramente al andar, ¡pero atiza un puñetazo que no veas!','images/img13.png',Colors.black),
+                    carta(context, '#78E8DB'  , 'Ice Wizard' ,'Este señor tan frío dispara fragmentos de hielo a sus enemigos y ralentiza su velocidad de movimiento y de ataque. Aunque él sea un témpano de hielo, su bigote hace que suba la temperatura en la arena.','images/img5.png',Colors.black),
+                    carta(context, '#E861A2'  , 'Hunter' ,'Genera un gran daño mientras más cerca está de su objetivo. Todo lo que le falta en precisión lo tiene escondido en sus pobladas cejas.','images/img6.png',Colors.white),
+                    carta(context, '#6FFC84'  , 'Golem' ,'Es lento, pero resistente, y solo ataca las estructuras. Cuando se destruye, explota y se convierte en dos golemitas que infligen daño de área.','images/golem.png',Colors.black),
+                    carta(context, '#6F94FC'  , 'Lava Hound' ,'El sabueso de lava es una majestuosa bestia voladora que ataca estructuras. Los cachorros son un poco menos majestuosos, pero igual de bestias... y atacan a cualquier cosa.','images/img8.png',Colors.white),
+                    carta(context, '#F2F5FF'  , 'Hog Rider' ,'Unidad cuerpo a cuerpo rápida que ataca las estructuras y puede saltar el río. Sigue la llamada del montapuercos de una puerta de la arena a la otra.','images/img9.png',Colors.black),
+                    carta(context, '#FF91F5'  , 'Barbarians' ,'Una horda de atacantes cuerpo a cuerpo con un perverso bigote y un caracter aún peor.','images/gladiador.png',Colors.black),
+
+
+
+                  ],
+                )
+              ),
+              //---------------------Usuario---------------------------
+
+
+
+
+
+
+
+
+            ],
+          )
+        ],
+      ),
+
+
+    );
+  }
+}
